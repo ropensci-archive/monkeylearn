e3bd55fa7f640e68dd449c49198e6ede9ea36397
aedobbyn <adobbs11@gmail.com>
2018-03-30 14:03:57 -0400

    clean up last instance of long string. closes #107

--- tests/testthat/test_monkey_outputs.R
+++ tests/testthat/test_monkey_outputs.R
@@ -80,7 +80,7 @@ testthat::test_that("We can use different texts_per_req in classify_df and get t
   text_4 <- "I hate not having any toothpaste."
   request_df <- tibble::tibble(
     txt = c(text1, text2, text3, text_4),
-    other_col = 1:4   # Add another column to make sure we keep this around
+    other_col = 1:4 # Add another column to make sure we keep this around
   )
 
   # General test of dataframe
